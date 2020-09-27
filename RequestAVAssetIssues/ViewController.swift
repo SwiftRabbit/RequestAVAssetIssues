b4362d3c065e035aa0c1d969175878367f7c825c
Leopold Roitel <leopoldroitel@fb.com>
2020-09-27 06:14:04 -0700

    Initial Commit

---
+++ RequestAVAssetIssues/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RequestAVAssetIssues
+//
+//  Created by leopoldroitel on 9/27/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
